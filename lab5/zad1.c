c33a34781a5a163e9303d529bfd868ddbcb5c450
Przemysław Galicki <pgalicki2@p109-18.labpk.inf.ug.edu.pl>
2016-03-18 11:21:55 +0100

    siemanko witam w moijej funkcji

---
+++ lab5/zad1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void divide(int n, int k, int *q, int*r){
+	*q=n/k;
+	*r=n%k;
+	}
+void main(){
+	int a,b,iloraz, reszta;
+	printf("podaj dwie liczby które chcesz podziielić:\n");
+	scanf("%d %d", &a, &b);
+	divide(a,b,&iloraz, &reszta);
+	printf("%d %d \n", iloraz, reszta );
+}
